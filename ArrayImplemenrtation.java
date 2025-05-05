398884686c5f439d84e2e188b7e30905db7b760c
Syed Ghulam Rasool <146963134+Syed-Ghulam@users.noreply.github.com>
2025-05-05 11:45:59 +0530

    Add files via upload

---
+++ ArrayImplemenrtation.java
@@ -0,0 +1,22 @@
+public class ArrayImplemenrtation {
+
+	public static void main(String[] args) {
+		//initialize the array
+		int[] arr=new int[5];
+		//value assigned
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		
+		//accessing the array
+		for (int i=0;i<5;i++)
+		{
+			System.out.println("Element at index"+i+":"+arr[i]);
+		}
+
+	}
+
+}
+
